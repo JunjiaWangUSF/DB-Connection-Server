f8928a72cb4cb02c095850c0e4f917e4247d486a
Junjia Wang <jwang268@dons.usfca.edu>
2023-07-27 14:10:21 -0400

    init

---
+++ src/main/java/com/example/dbconnectionserver/DbConnectionServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbconnectionserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbConnectionServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbConnectionServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dbconnectionserver/DbConnectionServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbconnectionserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbConnectionServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
